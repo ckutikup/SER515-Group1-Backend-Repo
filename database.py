384c6c05d9ff8b9a11f3bf62e290c5190f9f4103
bradhak5 <bradhak5@asu.edu>
2025-11-19 10:35:52 -0700

    Comment Fix For Story 61

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 
 # The format for a passwordless connection is "username:@"
 # The database name at the end is 'agile_db'.
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:Astroboy112100@127.0.0.1/agile_db"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:@127.0.0.1/agile_db"
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
